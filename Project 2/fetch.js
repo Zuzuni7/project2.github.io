b37bd2a33010d035fe6528c320ce59686beff2dc
Your Name <you@example.com>
2019-07-18 10:17:13 -0600

    committing what I have as of today

--- Project 2/fetch.js
+++ Project 2/fetch.js
@@ -1,6 +1,6 @@
 const baseUrl = "https://db.ygoprodeck.com/api/v4/cardinfo.php?";
 var searchItem = "";
-
+var search = "";
 function getJSON(url) {
     console.log(baseUrl+url);// prints full url
     return fetch(baseUrl+url) //return concat on the baseUrl...
@@ -19,34 +19,40 @@ function getJSON(url) {
 
 function searchCards(newUrl) {
     // The value in the search bar is updated now
+    if (newUrl == null)
+    {
+        newUrl="";
+    }
     searchItem = document.getElementById("textEntry").value;
     console.log(searchItem);
     showCards(newUrl + searchItem);
 }
 
-//MODEL
+
 function getCards(url) {
     console.log(url);
     return getJSON(url);
 }
 
-//VIEW
-function renderCardList(cards,cardListElement) {
-    const list = cardListElement.children[1];
+function renderCardList(cards,cardListElement,count) {
+    const list = cardListElement;
     list.innerHTML = '';
     // Cards is the second
     // 
+    
     cards.forEach(function(card) {
         // this also opens each item of the array in the array passed in
         //create elements for list <tr>
         //console.log(card.image_url);
-        let listItem = document.createElement('tr');
+
+        let listItem = document.createElement('h2');
         listItem.innerHTML = `
-        <td><img src="${card.image_url_small}"><a href="${card.image_url}">${card.name}</a></td>
-        <td>${card.type}</td>
-        <td>${card.desc}</td>
-        <td>${card.attribute}</td>
-        <td>${card.race}</td>
+        <img id="cardimg" src="${card.image_url}"> <br/>
+        <a href="${card.image_url}">${card.name}</a> 
+        <p>Type: ${card.type}</p><br/>
+        <div id="desc"><p >Card Description: ${card.desc}</p></div><br/>
+        <p class="attrib">Attribute: ${card.attribute}</p><br/>
+        <p>Race: ${card.race}</p>
         `;
         // listItem.addEventListener('click', function(event){
         //     event.preventDefault();
@@ -73,7 +79,7 @@ function showCards(url) {
       
         //get the list elements
         const cardListElement = document.getElementById('cardList');
-      
+        var paginated = results; 
         if (results == undefined)
         {
             results = [];
@@ -83,28 +89,29 @@ function showCards(url) {
         {
             // We must search the index of each arrray location bc it is a 
             // 2 dimensional array. results is an array of data in an array.
+            var count = 0;
             for (let i = 0; i < results.length; i++)
             {
-                renderCardList(results[i], cardListElement);
-            }
-            //console.log(results.length);
-            // The length will always be 0 unless all data from the API is pulled.
-            // A new array is created per 9,999+ and 99+
-            
-            //enable next and previous buttons
-            if (results.next) {
-                const next = document.getElementById('next');
-                next.ontouchend = () => {
-                    showCards(results.next);
-                    console.log("Going to the next!");
-                };
-            }
-            if (results.previous) {
-                const prev = document.getElementById('prev');
-                prev.ontouchend = () => {
-                    showCards(results.previous);
-                    console.log("Going to the previous!");
-                };
+                count++;
+                renderCardList(results[i], cardListElement,count);
+                // The length will always be 0 unless all data from the API is pulled.
+                // A new array is created per 9,999+ and 99+
+                
+                //enable next and previous buttons
+                if (results[i].next) {
+                    const next = document.getElementById('next');
+                    next.ontouchend = () => {
+                        showCards(results.next);
+                        console.log("Going to the next!");
+                    };
+                }
+                if (results[i].previous) {
+                    const prev = document.getElementById('prev');
+                    prev.ontouchend = () => {
+                        showCards(results.previous);
+                        console.log("Going to the previous!");
+                    };
+                }
             }
         }
         
@@ -119,28 +126,60 @@ function getCardDetails(url) {
 
 //clearData();
 
+//document.getElementById("")
+
+document.getElementById("prev").addEventListener("click", () => {
+    //window.history.back();
+    console.log("Fetch previous 10 cards");
+});
+
+document.getElementById("next").addEventListener("click", () => {
+    //window.history.forward();
+    console.log("Fetch next 10 cards");
+});
+
+// search called here
+document.getElementById("search").addEventListener("click", () => {
+    console.log(search);
+    searchCards(search);
+});
+/******************************************************************* */
+//Search type declarations are the 4 listed below. 
+document.querySelector("#keywordSearch").style.color = "";
+
 document.getElementById("keywordSearch").addEventListener("click", () => {
     console.log("Search By Card Name");
-    let search = "name=";
-    searchCards(search);
+    search = "name=";
+    //searchCards(search);
+    //Trying to alternate the colors of selected search types
+    // let style = document.querySelector("#keywordSearch").style;
+    // if (style.color == "red")
+    // {
+    //     style.color = "darkgoldenrod";
+    // }
+    // else if (style.color === "darkgoldenrod")
+    // {
+    //     style.color = "red";
+    // }
+    
 });
 
 document.getElementById("archetypeSearch").addEventListener("click", ()=> {
     console.log("Search By Archetype");
-    let search = "archetype=";
-    searchCards(search);
+    search = "archetype=";
+   // searchCards(search);
 });
 
 document.getElementById("typeSearch").addEventListener("click", ()=> {
     console.log("Search By Type");
-    let search = "type=";
-    searchCards(search);
+    search = "type=";
+    //searchCards(search);
 });
 
 document.getElementById("raceSearch").addEventListener("click", ()=> {
     console.log("Search By Race");
-    let search = "race=";
-    searchCards(search);
+    search = "race=";
+    //searchCards(search);
 });
 
 
